f3d6eb941a92bb8533b8d2737ee2e8c99114c080
detlefarend <detlef@arend-privat.net>
2024-02-15 10:09:28 +0100

    Initial setup #1

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 
-setup(name='mlpro_int_scikit_learn',
-version='1.0.0',
+setup(name='mlpro-int-scikit-learn',
+version='0.1.0',
 description='MLPro: Integration Scikit-learn',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
 license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
-packages=['mlpro_int_scikit_learn'],
+packages=['mlpro_int_sklearn'],
 
 # Package dependencies for full installation
 extras_require={
