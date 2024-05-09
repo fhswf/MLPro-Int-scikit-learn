d75fe457ca90fc8dd75225ff237217f8727a53e5
detlefarend <detlef@arend-privat.net>
2024-05-09 06:43:01 +0200

    Minimum version of MLPro set to 1.4.3

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-scikit-learn',
-version='0.1.1',
+version='0.1.2',
 description='MLPro: Integration scikit-learn',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
@@ -12,7 +12,7 @@ packages=['mlpro_int_sklearn'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "mlpro[full]>=1.4.1",
+        "mlpro[full]>=1.4.3",
         "scikit-learn>=1.4.1"
     ],
 },
